a056228c23345835bed147743a3dcaad1dc4cfc9
levleyfer <157710752+levleyfer@users.noreply.github.com>
2024-06-27 12:17:08 +0300

    Update app.js

--- ex3/app.js
+++ ex3/app.js
@@ -1,94 +1,93 @@
-const header = document.querySelector('h1');
-const app = document.getElementById('app');
-const ddMenu = document.querySelector('#ddMenu');
-const sandwitch = document.querySelectorAll('svg');
-const html = document.documentElement;
+const header = document.querySelector('h1')
+const app = document.getElementById('app')
+const ddMenu = document.querySelector('#ddMenu')
+const sandwitch = document.querySelectorAll('svg')
+const html = document.documentElement
 
 const toggleTheme = () => {
     const isDarkMode = html.classList.contains('dark');
     html.classList.toggle('dark');
 };
-
 const setView = (v) => {
-    header.innerText = v;
-    toggleMenu(true);
+    header.innerText = v
+    toggleMenu(true)
 
     if (v === 'Calculator') {
-        renderCalculator();
+        renderCalculator()
     } else if (v === 'About') {
-        renderAbout();
+        renderAbout()
     } else if (v === 'Contact') {
-        renderContact();
+        renderContact()
     }
-};
+}
 
 const toggleMenu = (hide) => {
     if (!hide) {
-        ddMenu.classList.toggle('hidden');
+        ddMenu.classList.toggle('hidden')
         document.querySelectorAll('svg').forEach((el) => {
-            el.classList.toggle('hidden');
-        });
+            el.classList.toggle('hidden')
+        })
     } else {
-        ddMenu.classList.add('hidden');
-        document.querySelectorAll('svg')[0].classList.remove('hidden');
-        document.querySelectorAll('svg')[1].classList.add('hidden');
+        ddMenu.classList.add('hidden')
+        document.querySelectorAll('svg')[0].classList.remove('hidden')
+        document.querySelectorAll('svg')[1].classList.add('hidden')
     }
-};
+}
 
 const addRow = (container, content) => {
-    const row = <div class='grid grid-cols-5 gap-2'>${content}</div>;
-    container.insertAdjacentHTML('beforeend', row);
-};
+    const row = `<div class='grid grid-cols-5 gap-2'>${content}</div>`
+    container.insertAdjacentHTML('beforeend', row)
+}
 
 const addMonitor = (container, text) => {
-    const t = text ?? '';
+    const t = text ?? ''
     const monitor = `
     <div id='monitor' class="bg-white dark:bg-gray-800 border-4 border-blue-400 dark:border-[#051227] h-20 flex items-center col-span-5 text-blue-800 dark:text-white p-2 rounded-lg mb-2 font-bold text-4xl">
         ${t}
     </div>`;
-    container.insertAdjacentHTML('beforeend', monitor);
-};
+    container.insertAdjacentHTML('beforeend', monitor)
+}
 
 const button = (text) => {
-    const c = text === 'calculate' ? 'col-span-4' : '';
-    return <div class='bg-blue-400 dark:bg-[#051227] hover:bg-blue-600 dark:hover:bg-[#091f3a] text-white ${c} py-1 rounded-md text-center text-lg font-bold cursor-pointer d-btn'>${text}</div>;
-};
+    const c = text === 'calculate' ? 'col-span-4' : ''
+    return `<div class='bg-blue-400 dark:bg-[#051227] hover:bg-blue-600 dark:hover:bg-[#091f3a] text-white ${c} py-1 rounded-md text-center text-lg font-bold cursor-pointer d-btn'>${text}</div>`
+}
 
 const addButtons = (container, nums) => {
-    const btnHTML = nums.map((n) => button(n)).join('');
-    addRow(container, btnHTML);
-};
+    const btnHTML = nums.map((n) => button(n)).join('')
+    addRow(container, btnHTML)
+}
 
 const click = (event) => {
-    const monitor = document.getElementById('monitor');
-    const bac = monitor.innerText.trim();
-    const a = event.target.innerText;
-    console.log(a);
+    const monitor = document.getElementById('monitor')
+    const bac = monitor.innerText.trim()
+    const a = event.target.innerText
+    console.log(a)
     if (a === 'clear') {
-        monitor.innerText = '';
+        monitor.innerText = ''
     } else if (a === 'calculate') {
-        monitor.innerText = bac + '=' + eval(bac);
+        monitor.innerText = bac + '=' + eval(bac)
     } else {
-        monitor.innerText += a;
+        monitor.innerText += a
     }
-};
+}
 
 const renderCalculator = () => {
-    const labels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '+', '-', '', '/', '*', 'calculate', 'clear'];
-    app.innerHTML = '';
-    addMonitor(app);
-    addButtons(app, labels);
-    const buttons = document.querySelectorAll('.d-btn');
-    buttons.forEach((el) => el.addEventListener('click', click));
-};
+    const labels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '+', '-', '*', '/', '**', 'calculate', 'clear']
+    app.innerHTML = ''
+    addMonitor(app)
+    addButtons(app, labels)
+    const buttons = document.querySelectorAll('.d-btn')
+    buttons.forEach((el) => el.addEventListener('click', click))
+}
 
 const renderAbout = () => {
-    app.innerHTML = '<div class="p-4 h-[200px] flex items-center justify-center">Temp for About</div>';
-};
+    app.innerHTML = '<div class="p-4 h-[200px] flex items-center justify-center">Temp for About</div>'
+}
 
 const renderContact = () => {
-    app.innerHTML = '<div class="p-4 h-[200px] flex items-center justify-center">Temp for Contact</div>';
-};
+    app.innerHTML = '<div class="p-4 h-[200px] flex items-center justify-center">Temp for Contact</div>'
+}
 
 const renderMenu = () => {
     // Initialize the menu state
@@ -126,6 +125,8 @@ const renderMenu = () => {
         button.addEventListener('click', () => toggleMenu());
     });
 };
+
+
 
 const renderThemeToggle = () => {
     const toggleButtons = [
