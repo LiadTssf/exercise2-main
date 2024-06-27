ade4010a29b344c21127b9ef0437982837738baa
levleyfer <157710752+levleyfer@users.noreply.github.com>
2024-06-27 12:02:00 +0300

    Update app.js

--- ex3/app.js
+++ ex3/app.js
@@ -4,7 +4,7 @@ const ddMenu = document.querySelector('#ddMenu')
 const sandwitch = document.querySelectorAll('svg')
 const html = document.documentElement
 
-const toggleTheme = () => {
+const toggle = () => {
     const isDarkMode = html.classList.contains('dark');
     html.classList.toggle('dark');
 };
