23b069ea5b94844253a31801c4ab920b1d15142d
Maxim Cherepanov <semaximche@gmail.com>
2024-07-11 20:16:10 +0300

    moved index.js, made all 4 width tabs

--- habit-tracker/README.md
+++ habit-tracker/README.md
@@ -3,18 +3,22 @@
 Habit tracking web app designed for web course.
 
 # Usage
+
 Before anything:
+
 ```
 cd habit-tracker
 ```
 
 Run vite server:
+
 ```
 npm install
 npm run dev
 ```
 
 Run prettier:
+
 ```
 npx prettier -w src
 ```
--- habit-tracker/src/HabitStatus.jsx
+++ habit-tracker/src/HabitStatus.jsx
@@ -1,21 +1,29 @@
-import React from 'react';
+import React from "react"
 
 const HabitStatus = ({ statuses }) => {
-  return (
-    <div className="bg-white rounded-lg shadow-md p-6 w-full md:w-1/2">
-      {statuses.map((item, idx) => (
-        <div
-          key={idx}
-          className={`flex items-center justify-between p-2 rounded-md mb-2 ${
-            item.completed ? 'bg-green-200 text-green-700' : item.status ? 'bg-red-200 text-red-700' : ''
-          }`}
-        >
-          <span>{item.habit}</span>
-          {item.status ? <span>{item.status}</span> : <button className="text-blue-600">Mark Complete</button>}
+    return (
+        <div className="w-full rounded-lg bg-white p-6 shadow-md md:w-1/2">
+            {statuses.map((item, idx) => (
+                <div
+                    key={idx}
+                    className={`mb-2 flex items-center justify-between rounded-md p-2 ${
+                        item.completed
+                            ? "bg-green-200 text-green-700"
+                            : item.status
+                              ? "bg-red-200 text-red-700"
+                              : ""
+                    }`}
+                >
+                    <span>{item.habit}</span>
+                    {item.status ? (
+                        <span>{item.status}</span>
+                    ) : (
+                        <button className="text-blue-600">Mark Complete</button>
+                    )}
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
-};
+    )
+}
 
-export default HabitStatus;
+export default HabitStatus
